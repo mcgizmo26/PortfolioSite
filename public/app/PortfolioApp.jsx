820733c98c92dcbaa37b68d5896d4f619b617d25
Lonnie McGill <mcgizmo_26@yahoo.com>
2016-10-14 09:56:18 -0600

    First commit

---
+++ public/app/PortfolioApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var PortfolioApp = React.createClass({
+  render() {
+    return (
+      <div></div>
+    )
+  }
+})
+
+ReactDOM.render(<PortfolioApp />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	entry: {
+		main: './public/app/ReactApp.jsx'
+	},
+	output: {
+		filename: 'bundle.js',
+		path: './public/scripts'
+	},
+	devtool: 'sourcemap',
+	module: {
+		loaders: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: 'babel'
+			},
+			{
+				test: /\.s?css$/,
+				exclude: /node_modules/,
+				loaders: ["style", "css", "sass"]
+			}
+		]
+	}
+};
